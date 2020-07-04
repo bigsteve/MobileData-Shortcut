aa3f8543806697b2e46caaf726699a487178e221
Stefan Miller <usstefanmiller@yahoo.com>
2020-07-03 21:46:58 -0500

    Update MobileData.sh

--- MobileData.sh
+++ MobileData.sh
@@ -1,5 +1,5 @@
 #1.  install android platform tools
 #2. open cmd and run:
-C:\< path-to-your-adb.exe - probably C:\Users\username\platform-tools >\adb shell
+C:\Users\Username\platform-tools\adb shell
 # into adb shell run:
 settings put secure sysui_qs_tiles "MobileData,$(settings get secure sysui_qs_tiles)"
