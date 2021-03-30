24f3802402844f0792a289e8810dcc79fc376cd0
Stefan Miller <usstefanmiller@yahoo.com>
2021-03-30 15:32:30 -0500

    steps clarifications and details added
    steps clarifications and details added

--- MobileData.sh
+++ MobileData.sh
@@ -1,5 +1,7 @@
 #1.  install android platform tools
-#2. open cmd and run:
+#2 enable settings> developer tools > usb debugging on your android phone
+#3. open cmd and run:
 C:\Users\Username\platform-tools\adb shell
-# into adb shell run:
+#4 into adb shell run:
 settings put secure sysui_qs_tiles "MobileData,$(settings get secure sysui_qs_tiles)"
+#5 disable usb debugging (recommended)
