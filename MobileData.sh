f9637a700aafffe04d387810fd6f01412752a422
Stefan Miller <usstefanmiller@yahoo.com>
2021-03-30 15:32:57 -0500

    steps clarifications and details added
    steps clarifications and details added

--- MobileData.sh
+++ MobileData.sh
@@ -4,4 +4,4 @@
 C:\Users\Username\platform-tools\adb shell
 #4 into adb shell run:
 settings put secure sysui_qs_tiles "MobileData,$(settings get secure sysui_qs_tiles)"
-#5 disable usb debugging (recommended)
+#5 disable usb debugging (recommended step)
